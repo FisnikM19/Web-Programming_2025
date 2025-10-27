de96e9175a62712d547e0a382be5f55ab7a9bc75
Fisnik Mamuti <138901011+FisnikM19@users.noreply.github.com>
2025-10-28 00:24:38 +0100

    auditory-2: added interface for InMemoryCategoryRepository

--- Auditories/Aud2/wp-2025/src/main/java/mk/ukim/finki/wp2025/repository/InMemoryCategoryRepository.java
+++ Auditories/Aud2/wp-2025/src/main/java/mk/ukim/finki/wp2025/repository/InMemoryCategoryRepository.java
@@ -1,45 +1,18 @@
 package mk.ukim.finki.wp2025.repository;
 
-
-import mk.ukim.finki.wp2025.bootstrap.DataHolder;
 import mk.ukim.finki.wp2025.model.Category;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-// Repository for handling the in-memory storage of categories
-@Repository
-public class InMemoryCategoryRepository {
-    public Category save(Category category) {
-        // Remove any existing category with the same name before adding (prevents duplicates)
-        // Note: We're using names as identifiers for simplicity
-        // In later lectures you'll learn why unique IDs are the better approach!
-        DataHolder.categories.removeIf(c -> c.getName().equals(category.getName()));
+public interface InMemoryCategoryRepository {
+    Category save(Category category);
 
-        DataHolder.categories.add(category);
+    List<Category> findAll();
 
-        return category;
-    }
+    Optional<Category> findByName(String name);
 
-    public List<Category> findAll() {
-        return DataHolder.categories;
-    }
+    List<Category> search(String text);
 
-    public Optional<Category> findByName(String name) {
-        return DataHolder.categories.stream()
-                .filter(c -> c.getName().equals(name))
-                .findFirst();
-    }
-
-    public List<Category> search(String text) {
-        return DataHolder.categories.stream()
-                .filter(c -> c.getName().contains(text) ||
-                        c.getDescription().contains(text))
-                .toList();
-    }
-
-    public void delete(String name) {
-        DataHolder.categories.removeIf(c -> c.getName().equals(name));
-    }
+    void delete(String name);
 }
